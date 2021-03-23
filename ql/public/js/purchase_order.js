1561113745426688acd7ceb8775579302a9cf70b
Perkasa JoB <perkasajob@gmail.com>
2021-03-23 09:01:39 +0000

    Supplier Release is added for QA

--- ql/public/js/purchase_order.js
+++ ql/public/js/purchase_order.js
@@ -3,3 +3,40 @@ frappe.listview_settings['Purchase Order'].refresh = function (listview) {
 		$( "span[title*='Grand Total:']").remove()
 	}
 }
+
+frappe.ui.form.on('Purchase Order', {
+	validate(frm){
+		check_supplier_release(frm)
+	}
+})
+
+function check_supplier_release(frm){
+
+	frappe.call({
+		method: 'frappe.model.db_query.get_list',
+		async: false,
+		args: {
+			doctype: "Supplier Release",
+			filters:{
+				'supplier': 'PT JAYATAMA SELARAS',//frm.doc.supplier,
+				'released': 0
+			},
+			fields:['item']
+		},
+		type: 'GET',
+		callback: function(o) {
+			var item_codes = frm.doc.items.map(o=>{return o.item_code})
+			let lock_items = ""
+			o.message.forEach(e => {
+				if(item_codes.includes(e.item)){
+					frappe.validated = false;
+					lock_items += e.item + ","
+				}
+				frappe.validated = false;
+				if(!frappe.validated){
+					frappe.msgprint(lock_items + " not yet released by QA")
+				}
+			});
+		}
+	});
+}
