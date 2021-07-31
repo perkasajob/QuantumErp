f362541bf9c9ca062143d8fccc1132bcf86d9662
root <root@erpnext.do>
2021-07-31 07:23:59 +0000

    adding date filters to Batch Qty

---
+++ ql/ql/report/batch_qty/batch_qty.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2021, Perkasa JoB and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Batch Qty"] = {
+	"filters": [
+	  {
+		"fieldname":"start_date",
+		"label": "Start Date",
+		"fieldtype": "Date",
+		"width": "80",
+		"reqd": 1,
+		"default": frappe.datetime.get_today(),
+	  },
+	  {
+		"fieldname":"end_date",
+		"label": "End Date",
+		"fieldtype": "Date",
+		"width": "80",
+		"reqd": 1,
+		"default": frappe.datetime.get_today(),
+	  },
+	//   {
+	// 	"fieldname":"batch_no_flt",
+	// 	"label": "Batch No",
+	// 	"fieldtype": "Link",
+	// 	"options": "Batch",
+	// 	"width": "80"
+	//   }
+	]
+  };
---
+++ ql/ql/report/batch_qty/batch_qty.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021, Perkasa JoB and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
