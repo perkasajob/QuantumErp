64c3a95851c5feb99941e2eab086855dcc58b2d1
root <root@erpnext.do>
2021-08-20 10:08:13 +0000

    PO on validate fix frm.doc.orderer

--- ql/public/js/purchase_order.js
+++ ql/public/js/purchase_order.js
@@ -4,9 +4,32 @@ frappe.listview_settings['Purchase Order'].refresh = function (listview) {
 	}
 }
 
+
 frappe.ui.form.on('Purchase Order', {
+	onload(frm){
+	    if(frm.doc.department){
+	        if (!(frappe.user.has_role("System Manager") || frappe.user.name =="Administrator") ){
+    	        frm.set_df_property("department", "read_only", 1);
+	        }
+	        return
+	    }
+	    var dept_list = []
+        frappe.db.get_single_value('QL Settings', 'dept_abbr')
+    	.then(value => {
+    		dept_list = value.split(",");
+    		var dept = dept_list.find((e)=>{return frappe.user.has_role(e)})
+    		if (!frm.doc.__islocal)
+    	        frm.set_df_property("department", "read_only", dept ? 1 : 0);
+        });
+	},
 	validate(frm){
+		if(!frm.doc.department){
+	        var msg = "Department must be filled"
+	        frappe.msgprint(msg);
+            throw msg;
+	    }
 		check_supplier_release(frm)
+		debugger
 		if (frm.doc.__islocal && !frm.doc.orderer){
 			frm.set_value("orderer", frappe.user.full_name())
 		}
