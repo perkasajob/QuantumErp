1fd6b1db5ff440ec6c0a517133aa59fbfbac158e
root <root@erpnext.do>
2021-08-27 16:33:27 +0000

    update fix

--- ql/ql/doctype/distributor_warehouse/distributor_warehouse.js
+++ ql/ql/doctype/distributor_warehouse/distributor_warehouse.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Distributor Warehouse', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.toggle_display(['address_html','contact_html'], !frm.doc.__islocal);
 
-	// }
+
+		if(!frm.doc.__islocal) {
+			frappe.contacts.render_address_and_contact(frm);
+
+		} else {
+			frappe.contacts.clear_address_and_contact(frm);
+		}
+		frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Distributor Warehouse'};
+
+	}
 });
--- ql/ql/doctype/distributor_warehouse/distributor_warehouse.py
+++ ql/ql/doctype/distributor_warehouse/distributor_warehouse.py
@@ -5,6 +5,8 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+from frappe.contacts.address_and_contact import load_address_and_contact
 
 class DistributorWarehouse(Document):
-	pass
+	def onload(self):
+		load_address_and_contact(self)
