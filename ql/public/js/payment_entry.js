8346b823f4d60e352853529d3bc8283bd25946ba
root <root@erpnext.do>
2022-03-31 02:54:50 +0000

    is_fixed_paid_amount is added to prevent override

--- ql/public/js/payment_entry.js
+++ ql/public/js/payment_entry.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Payment Entry', {
 	validate(frm){
-	    if(frm.doc.total_allocated_amount && !doc.frm.is_fixed_paid_amount){
+	    if(frm.doc.total_allocated_amount && !frm.doc.is_fixed_paid_amount){
 			frm.set_value("paid_amount", frm.doc.total_allocated_amount)
 		}
 	}
