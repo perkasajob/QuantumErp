c93c25dcc838f9fbb18417bc1b3287d55f208fb0
perkasajob <perkasajob@gmail.com>
2022-08-09 03:38:33 +0000

    sync changes on qs

--- ql/ql/transaction_base.py
+++ ql/ql/transaction_base.py
@@ -14,7 +14,6 @@ from six import string_types
 class QLTransactionBase(StatusUpdater):
 	def validate_rate_with_reference_doc(self, ref_details):
 		buying_doctypes = ["Purchase Order", "Purchase Invoice", "Purchase Receipt"]
-		frappe.msgprint("QLTransactionBase is CALLED ")
 
 		if self.doctype in buying_doctypes:
 			to_disable = "Maintain same rate throughout Purchase cycle"
@@ -28,7 +27,7 @@ class QLTransactionBase(StatusUpdater):
 				if d.get(ref_link_field):
 					ref_rate = frappe.db.get_value(ref_dt + " Item", d.get(ref_link_field), "rate")
 
-					if abs(flt(d.rate - ref_rate, d.precision("rate"))) >= .01:
+					if abs(flt(d.rate - ref_rate, d.precision("rate"))) >= 10:
 						frappe.msgprint(_("Row #{0}: Rate must be same as {1}: {2} ({3} / {4}) ")
 							.format(d.idx, ref_dt, d.get(ref_dn_field), d.rate, ref_rate))
 						frappe.throw(_("To allow different rates, disable the {0} checkbox in {1}.")
