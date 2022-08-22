48b8af01c42f2f663d562828ebbce0b66a5fe56d
perkasajob <perkasajob@gmail.com>
2022-08-22 03:00:36 +0000

    loading the original function into the customize

--- ql/public/js/purchase_receipt_list.js
+++ ql/public/js/purchase_receipt_list.js
@@ -1,5 +1,20 @@
-frappe.listview_settings['Purchase Receipt'].refresh = function (listview) {
-	if(!frappe.user.has_role(["Purchase User","Purchase Manager", "Accounts Manager", "Accounts User", "System Manager"])){
-		$( "span[title*='Grand Total:']").remove()
+frappe.listview_settings['Purchase Receipt'] = {
+	add_fields: ["supplier", "supplier_name", "base_grand_total", "is_subcontracted",
+		"transporter_name", "is_return", "status", "per_billed", "currency"],
+	get_indicator: function(doc) {
+		if(cint(doc.is_return)==1) {
+			return [__("Return"), "darkgrey", "is_return,=,Yes"];
+		} else if (doc.status === "Closed") {
+			return [__("Closed"), "green", "status,=,Closed"];
+		} else if (flt(doc.grand_total) !== 0 && flt(doc.per_billed, 2) < 100) {
+			return [__("To Bill"), "orange", "per_billed,<,100"];
+		} else if (flt(doc.grand_total) === 0 || flt(doc.per_billed, 2) == 100) {
+			return [__("Completed"), "green", "per_billed,=,100"];
+		}
+	},
+	refresh: function (listview) {
+		if(!frappe.user.has_role(["Purchase User","Purchase Manager", "Accounts Manager", "Accounts User", "System Manager"])){
+			$( "span[title*='Grand Total:']").remove()
+		}
 	}
-}
+};
