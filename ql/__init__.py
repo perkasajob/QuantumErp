02bfcbda51a2cfbcd3ffd42db5c5fbb1dfe57006
perkasajob <perkasajob@gmail.com>
2023-02-25 03:21:00 +0000

    User Allow Negative Stock is added

--- ql/__init__.py
+++ ql/__init__.py
@@ -2,14 +2,6 @@
 # Copyright (c) 2020, Quantum Labs
 from __future__ import unicode_literals
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 import frappe
-
-
-@frappe.whitelist()
-def get_logged_user_dept():
-	# if "SM" in frappe.get_roles(frappe.session.user):
-	# 	return True
-	return frappe.db.sql("select dept_code from `tabDepartment` dept left join `tabEmployee` emp ON emp.department = dept.name where emp.user_id='{0}'".format(frappe.session.user), as_dict=1)
-
